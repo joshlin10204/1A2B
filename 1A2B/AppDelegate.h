5f28c7aa097d3829936080657ae6c25948e5d990
joshlin10204 <joshlin10204@gmail.com>
2017-02-28 14:21:28 +0800

    Upload App

---
+++ 1A2B/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  1A2B
+//
+//  Created by Josh on 2015/6/2.
+//  Copyright (c) 2015年 Josh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
